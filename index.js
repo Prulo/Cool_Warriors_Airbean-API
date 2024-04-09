1d05b939e1c242068d6604a74d7f3800b0d473d7
Martin <martin.kolberg@outlook.com>
2024-04-09 13:26:29 +0200

    test from Martin branch

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express")
-const Datastore = require("nedb-promise")
+const express = require("express");
+const Datastore = require("nedb-promise");
 
 const PORT = 8000;
 const URL = "127.0.0.1";
 
 const db = new Datastore({
-    filename: "database.db",
-    autoload: true,
-  });
-  
-  const server = app.listen(PORT, URL, () => {
-    console.log(Listening to port: ${PORT});
-  });
+  filename: "database.db",
+  autoload: true,
+});
+
+const server = app.listen(PORT, URL, () => {
+  console.log(`Listening to port: ${PORT}`);
+});
