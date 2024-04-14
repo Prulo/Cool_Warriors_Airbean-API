54077a0e5239fad690baa637d20f6515e3c4fcf9
Martin <martin.kolberg@outlook.com>
2024-04-14 09:23:09 +0200

    revised structure, decided on not using m(v)c structure until goals for G is met

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const Datastore = require("nedb-promise");
+const { getMenu } = require("./routes/beans");
 
 const app = express();
 app.use(express.json());
 
 const PORT = 8000;
 const URL = "127.0.0.1";
+
+const userRoutes = require("./routes/users");
 
 const db = {};
 
@@ -17,6 +20,12 @@ const dbOrders = new Datastore({
   filename: "./database/orders.db",
   autoload: true,
 });
+const dbMenu = new Datastore({
+  filename: "./database/menu.db",
+  autoload: true,
+});
+
+/* app.use("/api/user", userRoutes); */
 
 const server = app.listen(PORT, URL, () => {
   console.log(`Listening to port: ${PORT}`);
@@ -25,3 +34,55 @@ const server = app.listen(PORT, URL, () => {
 app.get("/", (req, res) => {
   res.send("Hello there, my friend!");
 });
+// Hämtar menyn, separat funktion i nuvarande routes/beans men skall sannolikt flyttas/döpas om
+app.get("/beans/menu", async (req, res) => {
+  const singleMenuItem = { price, desc, id, title };
+  const { price, desc, id, title } = req.body;
+
+  try {
+    const menu = await getMenu();
+    res.status(200).send("Menu was successfully created");
+    res.json(menu);
+  } catch (err) {
+    res.status(500).send("Internal server error");
+  }
+});
+// Ej färdig, för att posta menyn till databasen
+app.post("/beans", async (req, res) => {
+  const { price, desc, id, title } = req.body;
+});
+
+// Skapa ny användare, skall lägga till handering för separat id-bibliotek, möjlighet att rodda "hashning" av lösenord
+app.post("/users/signup", async (req, res) => {
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    // id: bibliotek eller db-konsruerat?
+  };
+  try {
+    dbUsers.insert(user);
+    res.status(201).json({ message: "User created" });
+  } catch (err) {
+    res.status(400).send("Could not create a user");
+  }
+});
+// För att hitta användare med specifikt id
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await dbUsers.findOne({ _id: id }, (error, doc) => {
+      if (error) {
+        res.status(400).send("Person with that id was not found");
+      }
+      return doc;
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(404).send("Could not find user");
+  }
+});
+
+app.post();
+
+module.exports = { dbUsers, dbOrders };
--- routes/beans.js
+++ routes/beans.js
@@ -1,20 +1,19 @@
 const fs = require("fs").promises;
 
+//Satan i gatan, jag fick den att rendera om en kör nodemon routes/beans!!!
+
 const getMenu = async () => {
   try {
     const menuData = await fs.readFile("menu.json", "utf8");
+
     const menu = JSON.parse(menuData);
+    console.log(menuData);
     return menu;
   } catch (err) {
     console.log(err);
     throw err;
   }
 };
-(async () => {
-  try {
-    const menu = await getMenu();
-    console.log("Meny inläst:", menu);
-  } catch (error) {
-    console.error("Det gick inte att hämta menyn:", error);
-  }
-})();
+getMenu();
+
+module.exports = { getMenu };
