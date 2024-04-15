019d54bd805757da639ede88c98047912905ecb2
Martin <martin.kolberg@outlook.com>
2024-04-15 14:15:49 +0200

    Extended checking in signup request

--- index.js
+++ index.js
@@ -82,21 +82,41 @@ app.post("/add", async (req, res) => {
 
 // När vi skapar, rodda id, uuid,
 app.post("/users/signup", async (req, res) => {
-  const user = {
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-    // userId
-    // id: bibliotek eller db-konsruerat?
-  };
+  const { username, password, email } = req.body;
+
+  if (!username || !email) {
+    return res.status(400).send("Username and/or password are required");
+  }
   try {
-    dbUsers.insert(user);
-    res.status(201).json({ message: "User created" });
+    const existingUser = await dbUsers.findOne({
+      $or: [{ username }, { email }],
+    });
+    if (existingUser) {
+      if (existingUser.username === username) {
+        return res.status(400).json({ message: "Username already exists" });
+      } else {
+        return res.status(400).json({ message: "Email already exists" });
+      }
+    }
+
+    const newUser = {
+      username,
+      password,
+      email,
+      userId: uuidv4(),
+    };
+
+    await dbUsers.insert(newUser);
+    res.status(201).json({ message: "User created", userId: newUser.userId });
+    console.log(newUser);
     // Den kan dessvärre lägga till användare med tomma fält, lägg in ex "user.length > 0"
   } catch (err) {
     res.status(400).send("Could not create a user");
   }
 });
+
+app.get("/users/login");
+
 // För att hitta användare med specifikt id, anvämnda för att returnera ordrar lagda med ID:t?
 app.get("/users/:id", async (req, res) => {
   const id = req.params.id;
